3855f84b6fc3af608d469a8e4fad89e51aaae842
vaidya0303 <vaidya0304@gmail.com>
2022-10-24 15:11:11 +0530

    added interface and implemented in empwagecomputation class

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,6 +1,14 @@
 package com.bridgelab;
 
-class CompanyEmpWage {
+interface IEmployeeWageComputation
+{
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+
+    public void calculateTotalWage();
+}
+
+class CompanyEmpWage
+{
     // instance constants
     final String COMPANY_NAME;
     final int WAGE_PER_HR;
@@ -9,8 +17,8 @@ class CompanyEmpWage {
     // instance variable
     int totalEmpWage;
 
-    CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
-
+    CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
         COMPANY_NAME = companyName;
         WAGE_PER_HR = wagePerHr;
         MAX_WORKING_DAYS = maxWorkingDays;
@@ -23,6 +31,7 @@ class CompanyEmpWage {
         this.totalEmpWage = totalEmpWage;
     }
 
+    @Override
     public String toString()
     {
         System.out.println("Details of " + COMPANY_NAME + " employee");
@@ -34,7 +43,8 @@ class CompanyEmpWage {
     }
 }
 
-public class EmpWage {
+class EmpWage implements IEmployeeWageComputation
+{
     // class constants
     public static final int PART_TIME = 1;
     public static final int FULL_TIME = 2;
@@ -42,27 +52,27 @@ public class EmpWage {
     int noOfCompanies, index;
     CompanyEmpWage[] companies;
 
-    public EmpWage(int noOfCompanies) {
-
+    public EmpWage(int noOfCompanies)
+    {
         this.noOfCompanies = noOfCompanies;
         companies = new CompanyEmpWage[noOfCompanies];
         index = 0;
     }
 
-    void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
-
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
         companies[index++] = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
     }
 
-    int generateEmployeeType() {
-
+    int generateEmployeeType()
+    {
         return (int) (Math.random() * 100) % 3;
     }
 
-    int getWorkingHrs(int empType) {
-
-        switch (empType) {
-
+    int getWorkingHrs(int empType)
+    {
+        switch (empType)
+        {
             case FULL_TIME:
                 return 8;
             case PART_TIME:
@@ -72,38 +82,37 @@ public class EmpWage {
         }
     }
 
-    void calculateTotalWage() {
-
-        for (CompanyEmpWage company : companies) {
-
+    public void calculateTotalWage()
+    {
+        for (CompanyEmpWage company : companies)
+        {
             int totalWage = calculateTotalWage(company);
             company.setTotalEmployeeWage(totalWage);
             System.out.println(company);
         }
     }
 
-    int calculateTotalWage(CompanyEmpWage companyEmpWage) {
-
+    int calculateTotalWage(CompanyEmpWage companyEmpWage)
+    {
         System.out.println("Computation of total wage of " + companyEmpWage.COMPANY_NAME + " employee");
         System.out.println("-----------------------------------------------------");
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
         int workingHrs, totalWage = 0;
         for (int day = 1, totalWorkingHrs = 0; day <= companyEmpWage.MAX_WORKING_DAYS
-                && totalWorkingHrs <= companyEmpWage.MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
-
+                && totalWorkingHrs <= companyEmpWage.MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+        {
             int empType = generateEmployeeType();
             workingHrs = getWorkingHrs(empType);
             int wage = workingHrs * companyEmpWage.WAGE_PER_HR;
             totalWage += wage;
-
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
         return totalWage;
     }
 
-    public static void main(String args[]) {
-        
+    public static void main(String args[])
+    {
         EmpWage employeeWageComputation = new EmpWage(3);
         employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
         employeeWageComputation.addCompany("Google", 5, 40, 170);
