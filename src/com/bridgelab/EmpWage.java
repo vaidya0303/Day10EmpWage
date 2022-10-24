fb339d202d96de81cec346596274224d4bfd3561
vaidya0303 <vaidya0304@gmail.com>
2022-10-24 15:07:06 +0530

    added employee wage computation for multiple companies

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -2,24 +2,28 @@ package com.bridgelab;
 
 public class EmpWage {
 
-    public static void calculateTotalWage() {
-        //constant
+    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
+        // constant
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
-
         int totalWage = 0;
         int workingHrs = 0;
+
+        System.out.println("Details of " + companyName + " employee");
+        System.out.println("-----------------------------------------------------");
+        System.err.println("Wage per hour:" + wagePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkingHrs);
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
-        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+        for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
+                && totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs) {
 
             int empType = (int) (Math.random() * 100) % 3;
+
             /*
-            using switch statement
+            using switch case
              */
             switch (empType) {
                 case FULL_TIME:
@@ -32,16 +36,16 @@ public class EmpWage {
                     workingHrs = 0;
                     break;
             }
-            int wage = workingHrs * WAGE_PER_HR;
+
+            int wage = workingHrs * wagePerHr;
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
-
         }
-        System.out.println("Total wage for a month is " + totalWage);
+        System.out.println("Total wage for a month of " + companyName + " employee is " + totalWage + "\n");
     }
 
-    public static void main(String args[]){
-    
-        calculateTotalWage();
+    public static void main(String args[]) {
+        calculateTotalWage("Amazon", 40, 15, 200);
+        calculateTotalWage("BigBazar", 20, 20, 100);
     }
 }
