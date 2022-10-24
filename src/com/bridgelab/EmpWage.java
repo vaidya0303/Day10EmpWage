0d2c5827a8be4beab84461e86a4fbccc779e9852
vaidya0303 <vaidya0304@gmail.com>
2022-10-24 15:00:32 +0530

    added switch case inplace of if statements

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,25 +1,36 @@
+
 package com.bridgelab;
 
 public class EmpWage {
 
-        public static void main (String args[]) {
 
-            final int PART_TIME = 1;
-            final int FULL_TIME = 2;
-            final int WAGE_PER_HR = 20;
-            int empType = (int) (Math.random() * 100) % 3;
-            int workingHours = 0;
+    public static void main(String args[]) {
 
-            if (empType == FULL_TIME) {
-                System.out.println("Employee is Present Full time");
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours =0;
+
+        /*
+        using switch statment
+         */
+        switch (empType)
+        {
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");  //condition 1
                 workingHours = 8;
-            } else if (empType == PART_TIME) {
-                System.out.println("Employee is Present Part time");
+                break;
+
+            case PART_TIME:
+                System.out.println("Employee is Present Part time"); //condition 2
                 workingHours = 4;
-            } else {
+                break;
+
+            default:                                           //default statment if case 1 and 2 is fail then default is print
                 System.out.println("Employee is Absent");
-            }
-            int wage = workingHours * WAGE_PER_HR;
-            System.out.println("Employee Daily Wage is " + wage);
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
+}
