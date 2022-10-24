c738033fc417dd9fe578b6ee8c6beb11e46b03a5
vaidya0303 <vaidya0304@gmail.com>
2022-10-24 15:02:13 +0530

    added calculation of monthly wage

--- src/com/bridgelab/EmpWage.java
+++ src/com/bridgelab/EmpWage.java
@@ -1,36 +1,41 @@
-
 package com.bridgelab;
 
 public class EmpWage {
-
 
     public static void main(String args[]) {
 
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 3;
-        int workingHours =0;
+        final int WORKING_DAYS = 20;
 
-        /*
-        using switch statment
-         */
-        switch (empType)
-        {
-            case FULL_TIME:
-                System.out.println("Employee is Present Full time");  //condition 1
-                workingHours = 8;
-                break;
-
-            case PART_TIME:
-                System.out.println("Employee is Present Part time"); //condition 2
-                workingHours = 4;
-                break;
-
-            default:                                           //default statment if case 1 and 2 is fail then default is print
-                System.out.println("Employee is Absent");
+        int totalWage = 0;
+          
+        for (int day = 1; day <= WORKING_DAYS; day++){
+       
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHours =0;
+              
+              /*
+              using switch case
+              */
+              
+            switch (empType){
+            
+                case FULL_TIME:
+                    workingHours = 8;
+                    break;
+                        
+                case PART_TIME:
+                    workingHours = 4;
+                    break;
+                        
+                default:
+            }
+            int wage = workingHours * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + wage);
+            totalWage += wage;
         }
-        int wage = workingHours * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage is " + wage);
+        System.out.println("Total wage for a month is " + totalWage);
     }
 }
